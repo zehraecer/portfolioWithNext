94229b09b8f97c4b4d3ee6b8f0609c91fd1de3eb
Zehra Ecer <zehraecer1996@hotmail.com>
2026-06-03 17:12:47 +0300

    robot.js and sitemap.js added

---
+++ src/app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+    return {
+        rules: {
+            userAgent: "*",
+            allow: "/",
+        },
+        sitemap: "https://zehraecer.com.tr/sitemap.xml",
+    };
+}
---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+    return [
+        {
+            url: "https://zehraecer.com.tr",
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 1,
+        },
+    ];
+}
